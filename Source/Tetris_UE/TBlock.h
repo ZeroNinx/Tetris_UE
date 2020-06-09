537a4463412f0c05de05d2744a319927023f60b8
朱 蔚钦 <zwq939681378@sina.com>
2020-06-10 00:08:00 +0800

    JBlock旋转位置校正

---
+++ Source/Tetris_UE/TBlock.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Block.h"
+#include "TBlock.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TETRIS_UE_API ATBlock : public ABlock
+{
+	GENERATED_BODY()
+	
+};
