e4201b86d0dd7a6bb62e32137519a0195bf0e276
朱 蔚钦 <zwq939681378@sina.com>
2020-06-10 00:39:13 +0800

    T，L，J方块旋转初步完成

---
+++ Source/Tetris_UE/LBlock.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Block.h"
+#include "LBlock.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TETRIS_UE_API ALBlock : public ABlock
+{
+	GENERATED_BODY()
+	
+};
